ccc5410087d3109ef88591b2e2148542222e7d30
Берсенев Максим <bersenev.maks@gmail.com>
2022-05-02 22:21:50 +0500

    Solution 8 lesson, Robot-barmen

---
+++ 8_Robot-barmen/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * https://www.sololearn.com/learning/eom-project/1080/1053
+ */
+namespace _8_Robot_barmen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int drinks = Convert.ToInt32(Console.ReadLine());
+                int shelves = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(drinks / shelves);
+
+                Console.ReadKey();
+
+            }
+            /*
+             * 1. DivideByZeroException => "At least 1 shelf"
+             * 2. FormatException => "Please insert an integer"
+            */
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("At least 1 shelf");
+                Console.ReadKey();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please insert an integer");
+                Console.ReadKey();
+            }
+        }
+    }
+}
