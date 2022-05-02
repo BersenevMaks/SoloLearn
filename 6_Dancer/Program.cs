a717bf17da8a1fffb98b360da5be8b88f2a8d559
Берсенев Максим <bersenev.maks@gmail.com>
2022-05-02 15:37:07 +0500

    Add & Solution 6 lesson Dancer

---
+++ 6_Dancer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_Dancer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name1 = Console.ReadLine();
+            int points1 = Convert.ToInt32(Console.ReadLine());
+            string name2 = Console.ReadLine();
+            int points2 = Convert.ToInt32(Console.ReadLine());
+
+            DancerPoints dancer1 = new DancerPoints(name1, points1);
+            DancerPoints dancer2 = new DancerPoints(name2, points2);
+
+            DancerPoints total = dancer1 + dancer2;
+            Console.WriteLine(total.name);
+            Console.WriteLine(total.points);
+            Console.ReadKey();
+        }
+    }
+
+    class DancerPoints
+    {
+        public string name;
+        public int points;
+        public DancerPoints(string name, int points)
+        {
+            this.name = name;
+            this.points = points;
+        }
+
+        public static DancerPoints operator +(DancerPoints dancer1, DancerPoints dancer2)
+        {
+            return new DancerPoints(dancer1.name + " & " + dancer2.name, dancer1.points + dancer2.points);
+        }
+
+    }
+}
