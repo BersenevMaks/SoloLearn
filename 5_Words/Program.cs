df357f4a1060f90c7545e5675271aab2108360b4
Берсенев Максим <bersenev.maks@gmail.com>
2022-05-02 14:29:47 +0500

    Solution Words

---
+++ 5_Words/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+/*
+ * https://www.sololearn.com/learning/eom-project/1080/1046
+ */
+namespace _5_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = {
+                "home",
+                "programming",
+                "victory",
+                "C#",
+                "football",
+                "sport",
+                "book",
+                "learn",
+                "dream",
+                "fun"
+            };
+
+            string letter = Console.ReadLine();
+
+            int count = 0;
+
+            foreach (string s in words)
+            {
+                if (s.Contains(letter))
+                {
+                    Console.WriteLine(s);
+                    count++;
+                }
+            }
+            if (count < 1) Console.WriteLine("No match");
+            Console.ReadKey();
+
+        }
+    }
+}
