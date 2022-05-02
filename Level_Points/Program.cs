9ff220a4c2e438f58e417e599c4c711ddb2050bf
Берсенев Максим <bersenev.maks@gmail.com>
2022-05-02 14:14:10 +0500

    1. Rename solutions by numb at first 2. Create solution Social_Network

---
+++ Level_Points/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+/*
+ * https://www.sololearn.com/learning/eom-project/1080/1023
+ */
+namespace Level_Points
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int levels = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Points(levels));
+            Console.ReadKey();
+        }
+
+
+        static int Points(int levels)
+        {
+            int sum = 0;
+            while (levels > 0)
+                sum += levels--;
+            return sum;
+
+        }
+    }
+}
---
+++ Social_Network/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * https://www.sololearn.com/learning/eom-project/1080/1024
+ */
+namespace Social_Network
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
