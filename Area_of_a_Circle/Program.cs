3ac630642de956f4423b6956e6b679a997b85929
Берсенев Максим <bersenev.maks@gmail.com>
2022-05-02 13:34:30 +0500

    Solution

--- Area_of_a_Circle/Program.cs
+++ Area_of_a_Circle/Program.cs
@@ -10,6 +10,16 @@ namespace Area_of_a_Circle
     {
         static void Main(string[] args)
         {
+            const double pi = 3.14;
+            double radius = 0;
+            //if (args.Length > 0)
+            //{
+            //    radius = Convert.ToDouble(args[0]);
+            //}
+            //Console.WriteLine(radius * radius * pi);
+            radius = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(radius * radius * pi);
+            Console.ReadKey();
         }
     }
 }
