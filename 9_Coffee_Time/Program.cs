1bbddd474c5fb007225ada19e0b02779099cb28e
Берсенев Максим <bersenev.maks@gmail.com>
2022-05-03 07:44:02 +0500

    Solution 9 lesson Coffee Time

---
+++ 9_Coffee_Time/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_Coffee_Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int discount = Convert.ToInt32(Console.ReadLine());
+
+            Dictionary<string, int> coffee = new Dictionary<string, int>();
+            coffee.Add("Americano", 50);
+            coffee.Add("Latte", 70);
+            coffee.Add("Flat White", 60);
+            coffee.Add("Espresso", 60);
+            coffee.Add("Cappuccino", 80);
+            coffee.Add("Mocha", 90);
+
+
+            foreach (var v in coffee)
+                Console.WriteLine(v.Key + ": " + (v.Value - v.Value * discount/100));
+            Console.ReadKey();
+        }
+    }
+}
